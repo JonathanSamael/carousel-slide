8f8f7c4890b7cdb8db33d64b4be78f9f58d327fb
Jonathan Amaral <jonathansamael@live.com>
2022-10-31 14:26:50 -0300

    "Finalização do estilo das páginas, correção semanticas no HTML, passo a passo do JavaScript"

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+/* ---------- PASSO A PASSO -------------- 
+1 - Pegar a seta de avançar;
+
+2 - Identificar quando o usuário clicar no botão;
+
+3 - Quando o usuário clicar, passar para a próxima imagem;
+
+4 - Colocar a classe selected no cartão novo e retirar do outro para que seja escondido 
+
+5 - Repetir os passos para a seta de voltar;
+*/
+
