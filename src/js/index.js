eb616b4674b32f1fa0cd94e64b474114c4374cba
Jonathan Amaral <jonathansamael@live.com>
2022-10-31 21:42:59 -0300

    "Criação do JavaScript e interações com as setas"

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,52 @@
 5 - Repetir os passos para a seta de voltar;
 */
 
+const pages = document.querySelectorAll(".page");
+const btnGo = document.getElementById("btn-go");
+const btnBack = document.getElementById("btn-back");
+let currentPage = 0;
+
+function hiddenSelectedPage() {
+    const selectedPage = document.querySelector(".selected");
+    selectedPage.classList.remove("selected");
+}
+
+btnGo.addEventListener("click", () => {
+    if (currentPage === pages.length - 1) {
+        return;
+    }
+    currentPage++;
+    hiddenSelectedPage();
+    showPage();
+    arrowOpacity();
+})
+
+btnBack.addEventListener("click", () => {
+    if (currentPage === 0) {
+        return;
+    }
+    currentPage--;
+    hiddenSelectedPage();
+    showPage();
+    arrowOpacity();
+})
+
+function showPage() {
+    pages[currentPage].classList.add("selected");
+}
+
+function arrowOpacity() {
+    const notIsFirstPage = currentPage !== 0;
+    if (notIsFirstPage) {
+        btnBack.classList.remove("arrow-efect")
+    } else {
+        btnBack.classList.add("arrow-efect")
+    }
+
+    const isLastPage = currentPage !== 0 && currentPage === pages.length - 1;
+    if (isLastPage) {
+        btnGo.classList.add("arrow-efect");
+    } else {
+        btnGo.classList.remove("arrow-efect");
+    }
+}
